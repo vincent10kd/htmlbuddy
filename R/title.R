afa202a9c0f85774d1fa50118e2b2149ac1a24f9
Arina ten Cate - Hoek <arinetencate-hoek@Arinas-Air-2.fritz.box>
2024-10-16 23:38:40 +0200

    minor changes

--- R/title.R
+++ R/title.R
@@ -3,12 +3,14 @@
 #' @description Display text formatted as a title.
 #'
 #' @param text The title to embed in an HTML page.
+#' @param size One can choose between 'big' or anything else, in which case the font size will be smaller.
 #' @examples
 #' title('This is the title.')
 #'
 #' @export
 
-title <- function(text){
-  cat('<br><H2>',text,'</H2><br>')
+title <- function(text, size = 'big'){
+  if(size == 'big') cat('<H2>',text,'</H2>')
+  else cat('<H3>',text,'</H3>')
 }
 
