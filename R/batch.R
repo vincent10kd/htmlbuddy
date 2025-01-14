c2655a18d221a437a81227a844a30680aeb214cb
vincent10kd <vincent10kd@gmail.com>
2025-01-14 13:49:49 +0100

    fixed batch typo and added possibility to pass arguments

--- R/batch.R
+++ R/batch.R
@@ -4,6 +4,7 @@
 #'
 #' @param list The list object, containing objects (e.g., data.frames, character vectors, etc.)
 #' @param expr The function to be applied, e.g., tab, image, or text.
+#' @param ... Arguments to pass to the function to be applied.
 #' @param sep The separator argument for the internal paste() function, allowing one to separate tables using particular inputs, or to paste together a larger table if setting this to ''.
 #' @examples
 #' table_list <- list(head(iris), tail(iris))
@@ -13,6 +14,6 @@
 #'
 #' @export
 
-batch <- function(list, expr, sep = '<br><br>>'){
-  do.call('paste', c(lapply(list, expr), list(sep = sep)))
+batch <- function(list, expr, ..., sep = '<br><br>'){
+  do.call("paste", c(lapply(list, expr, ...), list(sep = sep)))
 }
