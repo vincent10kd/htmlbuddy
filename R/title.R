8e811b3aa3cc6ff708f21c27ce43bc090051fe1d
vincent10kd <vincent10kd@gmail.com>
2025-01-15 09:20:07 +0100

    adding collapsibility to divs

--- R/title.R
+++ R/title.R
@@ -5,15 +5,18 @@
 #' @param text The title to embed in an HTML page.
 #' @param size One can choose between 'big' or anything else, in which case the font size will be smaller.
 #' @param id Used to specify the div id of this title to which can be linked (e.g. from within a sidebar).
+#' @param collapsible When TRUE, allows the user to toggle contents (anything following the title) to be visible or invisible.
 #' @examples
 #' title('This is the title.')
 #'
 #' @export
 
-title <- function(text, size = 'big', id = NULL){
+title <- function(text, size = 'big', id = NULL, collapsible = TRUE){
   if(size == 'big') textObj <- paste0('<H2>', text, '</H2>')
   else textObj <- paste0('<H3>', text, '</H3>')
   if(!is.null(id)) textObj <- paste0('<div id = "',id,'">', textObj)
+  if(collapsible) textObj <- paste0('<details><summary>', textObj,'</p></summary><span>')
+  textObj <- paste0('</span></details>', textObj)
   class(textObj) <- 'pageObject'
   return(invisible(textObj))
 }
